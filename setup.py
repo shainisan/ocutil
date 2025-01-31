2002b163b659342e53c2f6be5208b7ccea7010f3
Shai Nisan <114759257+shainisan@users.noreply.github.com>
2025-01-31 17:45:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-# setup.py
 from setuptools import setup
 
 setup(
     name='ocutil',
     version='0.1.0',
-    py_modules=['ocutil'],  # Tells setuptools to treat ocutil.py as a module
+    py_modules=['ocutil'],  # Tells setuptools to treat ocutil.py as a module named 'ocutil'
     install_requires=[
         'oci'
     ],
     entry_points={
         'console_scripts': [
-            'ocutil=ocutil:main',  # "ocutil.py" must have a function "main()"
+            'ocutil=ocutil:main',  # 'ocutil.py' must have a function named 'main()'
         ]
     },
     python_requires='>=3.7',
