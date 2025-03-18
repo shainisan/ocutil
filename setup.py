377e7010ac3777dcf2e4983681195bcee2dad394
Shai Nisan <shaini@copyleaks.com>
2025-03-18 12:38:26 +0100

    v2 - see changelog

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocutil',
-    version='1.0.0',
+    version='2.0.0',
     description='Oracle Cloud Object Storage CLI utility',
     author='Shai Nisan',
     author_email='your.email@example.com',
