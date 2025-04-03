89f32949c4d8e3366a0841ec623c622c617e3914
Shai Nisan <shaini@copyleaks.com>
2025-04-03 10:29:51 +0200

    passphrase changes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocutil',
-    version='2.0.0',
+    version='2.0.1',
     description='Oracle Cloud Object Storage CLI utility',
     author='Shai Nisan',
     author_email='your.email@example.com',
