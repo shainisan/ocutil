cbef6f0a4c46a42f268c11156f73216b3c2cbb98
Shai Nisan <shaini@copyleaks.com>
2025-04-13 21:43:06 +0200

    passphrase input if missing

--- ocutil/utils/oci_manager.py
+++ ocutil/utils/oci_manager.py
@@ -1,5 +1,6 @@
 import os
 import oci
+import getpass
 
 class OCIManager:
     def __init__(self, config_profile='DEFAULT'):
@@ -13,9 +14,15 @@ class OCIManager:
             # Expand the home directory if needed.
             config_path = os.path.expanduser("~/.oci/config")
             config = oci.config.from_file(config_path, self.config_profile)
+            
+            # Check if pass_phrase is missing or empty and prompt the user if needed.
+            if 'pass_phrase' not in config or not config['pass_phrase']:
+                config['pass_phrase'] = getpass.getpass("Enter your OCI key passphrase: ")
+            
             return config
         except Exception as e:
             raise Exception(f"Error loading OCI config: {e}")
+
 
     def initialize_object_storage_client(self):
         try:
